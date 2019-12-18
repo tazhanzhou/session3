4cc3ff2e06a40293a9d627adabf483fa2a4674a4
tazhanzhou <tazhanzhou@gmail.com>
2019-12-17 20:29:56 -0500

    something

--- springsample/src/main/java/springsample/Person.java
+++ springsample/src/main/java/springsample/Person.java
@@ -2,12 +2,18 @@ package springsample;
 
 public class Person {
 	String firstName;
+	int age;
 	
 	public Person() {
 	
 	}
 	
+	public Person(int passAgeValue, String nameOfPerson) {
+		this.age = passAgeValue;
+		this.firstName = nameOfPerson;
+	}
+	
 	public void showMessage() {
-		System.out.println("I am a person");
+		System.out.println("I am a person, my name is "+ this.firstName+", and I am "+this.age+" years old.");
 	}
 }
